487da573c07fee02c5af316cf3fbae1d2827744e
Taurat-kibria <tauratkibria9@gmail.com>
2025-09-01 17:02:23 +0600

    still need little bit of finishing

--- script.js
+++ script.js
@@ -3,63 +3,67 @@
 
 const cards = document.getElementsByClassName('heart_icon')
 
-for(const card of cards){
+for (const card of cards) {
     card.addEventListener('click', function () {
         let heartCount = parseInt(document.getElementById('heart_count').innerText);
         heartCount = heartCount + 1;
         document.getElementById('heart_count').innerText = heartCount
-        
+
     })
-    }
+}
 
 
-    
-    // const callBtns = document.querySelectorAll('.call_btn')
-    // const cardTittle = document.getElementsByClassName('card_tittle')
-    // const cardNum = document.getElementsByClassName('card_num')
-    
-    const AllCard = document.getElementsByClassName('card');
-    
-    for(const tem of AllCard){
-        
-        tem.querySelector('.call_btn').addEventListener('click',function(){
-             const cardTittle = tem.querySelector('.card_tittle').innerText
-            const cardNum = tem.querySelector('.card_num').innerText
 
-            alert(cardTittle +"\nNumber: " + cardNum)
-            
-            // call button feature
-            let token = parseInt(document.getElementById('gold_count').innerText)
-            token = token - 20;
-            if(token < 20){
-                alert('Insufficient coins')
-            }
-            document.getElementById('gold_count').innerText = token
+const AllCard = document.getElementsByClassName('card');
 
-            
-            
+for (const tem of AllCard) {
 
-           
+    tem.querySelector('.call_btn').addEventListener('click', function () {
+        const cardTittle = tem.querySelector('.card_tittle').innerText
+        const cardNum = tem.querySelector('.card_num').innerText
 
-        })
+        alert(cardTittle + "\nNumber: " + cardNum)
 
-        
-    }
-    
+        // call button feature
+        let token = parseInt(document.getElementById('gold_count').innerText)
+        token = token - 20;
+        if (token < 20) {
+            alert('Insufficient coins')
+        }
+        document.getElementById('gold_count').innerText = token
 
-    
-    
-    // for(const btn of callBtns){
-    //     btn.addEventListener('click',function(){
-            
-    //         let token = parseInt(document.getElementById('gold_count').innerText)
-    //         token = token - 20;
-    //         if(token < 20){
-    //             alert('Insufficient coins')
-    //         }
-    //         document.getElementById('gold_count').innerText = token
-    //     })
-    // }
+        // call history funtionality
+
+        // document.getElementById('call_his_tit').innerText = cardTittle
+        // document.getElementById('call_his_num').innerText = cardNum
+
+        const time = new Date().toLocaleTimeString();
+
+        let chilEl = document.createElement('div')
+        chilEl = `
+            <div class="flex justify-between items-center bg-[#fafafa]
+             rounded-lg p-4 mt-4 ">
+
+                <div class="l">
+                    <h1 id="call_his_tit" class="text-lg font-semibold">
+                    ${cardTittle}</h1>
+                    <p id="call_his_num" class="text-[#5c5c5c] text-lg font-normal">
+                    ${cardNum}</p>
+                </div>
+
+                <div class="r">
+                    <p class="text-[#5c5c5c] text-lg font-normal">
+                    ${time} AM</p>
+                </div>
+            </div>
+            `
+        document.getElementById('call_his_parent').appendChild(chilEl)
+
+    })
+
+
+}
+
 
 
 
